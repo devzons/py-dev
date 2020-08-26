79a6d393ad1d69ee75b6daddff63f4561237797d
devzons <devzons@gmail.com>
2020-08-26 13:44:48 -0700

    create fixed size window

--- gui_dev/gui.py
+++ gui_dev/gui.py
@@ -1,4 +1,10 @@
 from tkinter import *
 
 root = Tk()
+root.title("Dev GUI")
+root.geometry("640x480")
+# root.geometry("640x480+100+300") # width * height + x-axis + y-axis
+
+root.resizable(False, False)
+
 root.mainloop()
