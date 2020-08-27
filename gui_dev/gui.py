033dca1f9202829c778fcf587ef1b9b234cd5939
devzons <devzons@gmail.com>
2020-08-27 14:40:22 -0700

    Button UI

--- gui_dev/gui.py
+++ gui_dev/gui.py
@@ -2,9 +2,27 @@ from tkinter import *
 
 root = Tk()
 root.title("Dev GUI")
-root.geometry("640x480")
-# root.geometry("640x480+100+300") # width * height + x-axis + y-axis
 
-root.resizable(False, False)
+btn1 = Button(root, text="Button 1")
+btn1.pack()
+
+btn2 = Button(root, padx=20, pady=10, text="Button 2")
+btn2.pack()
+
+btn3 = Button(root, width=10, height=3, text="Button 3")
+btn3.pack()
+
+btn4 = Button(root, fg="red", text="Button 4")
+btn4.pack() 
+
+# photo = PhotoImage(file="/Users/don/Projects/python/gui_dev/check.jpg")
+# btn5 = Button(root, image=photo)
+# btn5.pack()
+
+def btncmd():
+    print("Button clicked!")
+
+btn6 = Button(root, text="Working Button", command=btncmd)
+btn6.pack() 
 
 root.mainloop()
